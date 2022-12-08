bf0690a7bb5fac856f1aecde369c406fdd688775
Oleh <118124646+Olehq312@users.noreply.github.com>
2022-12-08 13:00:11 +0100

    08.12

--- js/java.js
+++ js/java.js
@@ -1,11 +1,18 @@
 const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navTwo = document.querySelector('.nav-two');
+    const worldIcon = document.querySelector('.language-selector');
+    const languages = document.querySelector('.lang-flag');
 
-    const worldIcon = document.querySelector('.icon');
-    const languages = document.querySelector('.flags');
+    //Toggle Navs
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navTwo.classList.toggle('nav-active');
 
-
-
-
+        //Burger Animation
+        burger.classList.toggle('burger-active');
+    });
 
     //Toggle Flags
     worldIcon.addEventListener('click', () => {
@@ -14,3 +21,5 @@ const navSlide = () => {
 }
 
 navSlide();
+
+
