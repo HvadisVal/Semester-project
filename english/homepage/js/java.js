bcdc1dfa873e79917e8fa3ad17ef440bae3cd2e6
TLNRB <nortol01@easv365.dk>
2022-12-14 11:59:04 +0100

    Added home page

---
+++ english/homepage/js/java.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navTwo = document.querySelector('.nav-two');
+    const worldIcon = document.querySelector('.language-selector');
+    const languages = document.querySelector('.lang-flag');
+    
+    //Toggle Navs
+    burger.addEventListener('click', () =>{
+      nav.classList.toggle('nav-active');
+      navTwo.classList.toggle('nav-active');
+  
+      //Burger Animation
+      burger.classList.toggle('burger-active');
+    });
+  
+    //Toggle Flags
+    worldIcon.addEventListener('click', () =>{
+      languages.classList.toggle('lang-flag-active');
+    });
+  }
+  
+  navSlide();
